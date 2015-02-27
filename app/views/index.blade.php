e5d6f6c8f085cd78ef5fa5006e25c2e53f2831fd
princessadam <princess.adamrebaula@gmail.com>
2015-02-27 13:48:33 +0800

    add other folders and files

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.default')
-
 @section('content')
 
-	HI
+	<div class="container page">
+		<div class="col-md-8">
+			Featured Video here
+		</div><!--/.col-md-8-->
+		<div class="col-md-4">
+			ads
+		</div><!--/.col-md-4-->
+	</div><!--/.container page-->
 
 @stop
