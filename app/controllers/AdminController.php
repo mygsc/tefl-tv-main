54c998e40cd967846491a62b3ae560f026a0d75e
Rommel Porras <r3mmel023@gmail.com>
2015-03-02 10:22:51 +0800

    Fixed validator error in login()

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -8,7 +8,7 @@ class AdminController extends BaseController {
 	public function postIndex(){
 		$input = Input::all();
 		$validate = Validator::make($input, array('username' => 'required','password' => 'required'));
-		if($validate->fails()) return Redirect::route('admins.index')->withFlashMessage($validate)->withInput();
+		if($validate->fails()) return Redirect::route('admin.index')->withInput()->withErrors($validate);
 		$attempt = Admin::getAuthLogin($input['username'], $input['password']);
 		if($attempt){
 			$verified = Auth::User()->verified;
@@ -25,4 +25,10 @@ class AdminController extends BaseController {
 		return Redirect::route('admin.index');
 	}
 
+	public function resetpassword(){
+		Mail::send('emails.auth.userNotification', ['userlink' => $verification_link, 'fname'=> ucfirst(Input::get('first_name')) ], function($message) use ($user)
+		{
+			$message->to($user->email, 'Mailusmail')->subject('Mailusmail Account Confirmation');
+		});
+	}
 }
