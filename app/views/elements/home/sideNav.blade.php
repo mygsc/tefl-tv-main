daee1ae3da2b37c712c4b7d4e4b5f18d73a5e720
princessadam <princess.adamrebaula@gmail.com>
2015-03-02 16:36:28 +0800

    create information pages

---
+++ app/views/elements/home/sideNav.blade.php
@@ -0,0 +1,11 @@
+
+    <span class="sidelinks">
+        <li>{{ link_to_route('homes.index', 'Home', null) }}</li>
+        <li>{{ link_to_route('homes.aboutus', 'About Us', null) }}</li>
+        <li>{{ link_to_route('homes.advertisements', 'Advertisement', null) }}</li>
+        <li>{{ link_to_route('homes.privacy', 'Privacy', null) }}</li>
+        <li>{{ link_to_route('homes.termsandconditions', 'Terms and Condition', null) }}</li>
+        <li>{{ link_to_route('homes.copyright', 'Copy Right', null) }}</li>
+    </span>
+    
+   <br/>
