e11bc7e0105bb7295f57c814eeb11533c27bab47
princessadam <princess.adamrebaula@gmail.com>
2015-03-05 14:46:43 +0800

    update subscribers

--- app/views/users/channel.blade.php
+++ app/views/users/channel.blade.php
@@ -129,7 +129,7 @@
 				  					&nbsp;
 				  					<video height="auto" width="100%" class="h-video controls>
 				  						<source src="/videos/{{$usersVideo->file_name}}.{{$usersVideo->extension}}" type="video/mp4" />		 
-				  						</video>
+				  					</video>
 				  						<div class="v-Info">
 				  							{{$usersVideo->title}}
 				  						</div>
@@ -161,20 +161,33 @@
 				    <div role="tabpanel" class="tab-pane" id="Subscribers">
 				    	<br/>
 				    	<div class="row">
-				    		@foreach($subscriberLists as $subscriberList)
-				    		<div class="subscribers">
-				    			<div class="col-md-6">
-				    				<img src="/img/user/u1.png" class="userRep2">&nbsp;
-				    				<span><b>{{$subscriberList->first_name}} {{$subscriberList->last_name}}</b></span>&nbsp;
-				    				<br/>&nbsp;
-				    				<span>w/ <b>2k</b> Subscribers</span>&nbsp;
-				    				<button class="btn btn-primary btn-xs pull-right">Subscribe</button>
-				    			</div>
-				    		</div>
-				    		@endforeach
-						</div>
-							
-				    </div>
+					    	<div class="col-md-10 col-md-offset-1">
+					    		<div class="col-md-6 pull-right">
+					    			<div class="input-group">
+					    				{{ Form::text('add', null, array('id' => 'category','required', 'placeholder' => 'Search Video', 'class' => 'form-control c-input ')) }}
+					    				<span class="input-group-btn">
+					    					{{ Form::submit('Search', array('id' => 'button', 'class' => 'btn btn-info ')) }}
+					    				</span>
+					    			</div>
+
+					    		</div>
+				    			<br/>
+				    			<hr/>
+									
+				    			@foreach($subscriberLists as $subscriberList)
+				    			<div class="subscribers">
+					    			<div class="col-md-6">
+					    				<img src="/img/user/u1.png" class="userRep2">&nbsp;
+					    				<span><b>{{$subscriberList->first_name}} {{$subscriberList->last_name}}</b></span>&nbsp;
+					    				<br/>&nbsp;
+					    				<span>w/ <b>2k</b> Subscribers</span>&nbsp;
+					    				<button class="btn btn-primary btn-xs pull-right">Subscribe</button>
+					    			</div>
+					    		</div>
+				    			@endforeach
+							</div>	
+				   		 </div>
+				   	</div>
 
 				    <div role="tabpanel" class="tab-pane" id="Subscriptions">
 				    		<br/>
