966bc22dda8f6d6f0c9caaff91a8c65930150dc7
Kevin Cabrera <cabrerakevinc@yahoo.com>
2015-03-11 09:26:13 +0800

    update

--- app/views/homes/popular.blade.php
+++ app/views/homes/popular.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.default')
 
 @section('content')
-	<div class="container page">
+<div class="container page">
 	<h1>Popular Videos</h1>
-	@foreach($latestVideos as $latestVideo)
-	<div class="col-md-3">
-		<img src="/img/thumbnails/v4.png">
+	@foreach($latestVideos as $key => $latestVideo)
+	<!-- 12 column / 3 column = 4 -->
+		<div class="col-md-3">
+			<img src="/img/thumbnails/v4.png">
 
-		<div class="v-Info">
-			<a href="{{route('homes.watch-video')}}">{{$latestVideo->title}}</a>
+			<div class="v-Info">
+				<a href="{{route('homes.watch-video')}}">{{$latestVideo->title}}</a>
+			</div>
+			<div class="count">
+				Channel: <a href="{{route('view.users.channel', array($latestVideo->channel_name))}}">{{$latestVideo->channel_name}}</a>
+				<br />
+				Views: {{$latestVideo->views}}, Likes: {{$latestVideo->likes}}
+				<br />
+				Date Uploaded: {{$latestVideo->created_at}}
+			</div>
 		</div>
-		<div class="count">
-			Channel: <a href="{{route('view.users.channel', array($latestVideo->channel_name))}}">{{$latestVideo->channel_name}}</a>
-			<br />
-			Views: {{$latestVideo->views}}, Likes: {{$latestVideo->likes}}
-			<br />
-			Date Uploaded: {{$latestVideo->created_at}}
-		</div>
-	</div>
+
 	@endforeach
-	</div>
+
+</div>
 @stop
