c792acf0b6af36b305e4317bc19791e3c8b5bab4
Rommel Porras <r3mmel023@gmail.com>
2015-03-11 16:26:40 +0800

    Fixed route intended for guest's Subscribe button

--- app/views/users/viewusers.blade.php
+++ app/views/users/viewusers.blade.php
@@ -35,14 +35,16 @@
 									<a href=""><img src="/img/icons/yt.png"></a>
 									<a href=""><img src="/img/icons/wl.png"></a>
  									
- 									
+ 									@if($user_id)
 										{{Form::open(array('route'=>'post.addsubscriber', 'id' =>'subscribe-userschannel'))}}
 						    				{{Form::hidden('user_id',$userChannel->id)}}
 						    				{{Form::hidden('subscriber_id', $user_id)}}
 						    				{{Form::hidden('status','subscribeOn')}}
 									    	{{Form::submit('Subscribe', array('class'=> 'btn btn-primary btn-xsp lull-right', 'id'=>'subscribebutton'))}}
 									    {{Form::close()}}
-
+									@else
+										{{link_to_route('homes.signin', 'Subscribe', '', array('class'=>'btn btn-primary btn-xsp lull-right')); }}
+								    @endif
 								</span>	
 							</div>
 						</div>	
