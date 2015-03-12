109dda058af94d374346c536c8b2a3b333f589cd
Aldrin Gauran <aldrin.gauran@gmail.com>
2015-03-12 09:49:31 +0800

    added new create playlists view

--- app/views/users/playlists.blade.php
+++ app/views/users/playlists.blade.php
@@ -1,1 +1,18 @@
-Playlists
+@extends('layouts.default')
+
+@section('content')
+
+	<div class="container">
+		<div class="row">
+			<div>
+				Create Playlists
+				<br/>
+				{{Form::open()}}
+					{{Form::label('title', 'Playlist Title: ')}} &nbsp; {{Form::text('title', null, array('class' => 'form-control'))}}
+					<br/>
+					{{Form::submit('Create Playlist')}}
+				{{Form::close()}}
+			</div>
+		</div>
+	</div>
+@stop
