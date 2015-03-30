c061ea207e0d221b557d7b8ea53ce9cfc276c727
Rommel Porras <r3mmel023@gmail.com>
2015-03-30 12:52:54 +0800

    Modified getUsersChannels()

--- app/views/elements/users/myChannelTabs/tab-Home.blade.php
+++ app/views/elements/users/myChannelTabs/tab-Home.blade.php
@@ -151,7 +151,7 @@
 							<a href="{{route('view.users.channel', $profile)}}">
 
 							<img src="/img/user/u1.png" class="userRep2"/>&nbsp;
-								<span><b>{{$profile}}>/b></span>
+								<span><b>{{$profile->channel_name}}</b></span>
 							</a>&nbsp;
 							<br/>&nbsp;
 							<span>w/ <b>{{$subscriberCount}}</b> Subscribers</span>&nbsp;
@@ -195,25 +195,12 @@
 							
 								<a href="{{route('view.users.channel', $profile1)}}">
 								<img src="/img/user/u1.png" class="userRep2">&nbsp;
-								<span><b>{{$profile1}}</b></span>
+								<span><b>{{$profile1->channel_name}}</b></span>
 								</a>&nbsp;
 								<br/>&nbsp;
-								<span>w/ <b>{{$subscriptionCount}}</b> Subscribers</span>&nbsp;
+								<span>w/ <b>{{count($subscriberCount)}}</b> Subscribers</span>&nbsp;
 								<!-- <button class="btn btn-unsub btn-xs pull-right">Unsubscribe</button> -->
-								<?php
-									$ifAlreadySubscribe = DB::table('subscribes')->where(array('user_id' => Auth::User()->id, 'subscriber_id' => $profile1->id))->first();
-								?>
-								{{Form::open(array('route'=>'post.addsubscriber', 'id' =>'subscribe-userChannel', 'class' => 'inline'))}}
-					    			{{Form::hidden('user_id', $profile->id)}}
-					    			{{Form::hidden('subscriber_id', Auth::User()->id)}}
-					    			@if(!$ifAlreadySubscribe)
-					    				{{Form::hidden('status','subscribeOn')}}
-								    	{{Form::submit('Subscribe', array('class'=> 'btn btn-primary btn-xs pull-right', 'id'=>'subscribebutton'))}}
-								    @else
-								    	{{Form::hidden('status','subscribeOff')}}
-								    	{{Form::submit('Unsubscribe', array('class'=> 'btn btn-primary btn-xs pull-right', 'id'=>'subscribebutton'))}}
-								    @endif
-								{{Form::close()}}
+								
 							</div>
 						</div>
 						@endforeach					
