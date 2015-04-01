aaf14b13018614bc8ce918e2528705f7ce59432a
Aldrin Gauran <aldrin.gauran@gmail.com>
2015-04-01 10:55:52 +0800

    modified error message in signup

--- app/models/User.php
+++ app/models/User.php
@@ -70,7 +70,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		'confirm_password' =>'same:password',
 		'first_name' => 'required|regex:/(^[A-Za-z]+$)+/',
 		'last_name' => 'required|regex:/(^[A-Za-z]+$)+/',
-		'contact_number' => 'regex:/(^[0-9]+$)+/');
+		'contact_number' => 'regex:/(^[+0-9]+$)+/');
 
 	public static $userPasswordRules = array(
 		'currentPassword' => 'required',
