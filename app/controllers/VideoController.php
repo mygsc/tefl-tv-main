fcf60e0adfbbfa9787a00cc5deb4125971cce567
Grald <gerald.burasca27@gmail.com>
2015-04-07 15:45:30 +0800

    added ffmpeg

--- app/controllers/VideoController.php
+++ app/controllers/VideoController.php
@@ -114,6 +114,21 @@ class VideoController extends Controller {
 		return View::make('users.addDescription',compact('videos'));
 	}
 	public function postAddDescription($id){
+
+		// $ffmpeg = FFMpeg\FFMpeg::create();
+		// $video = $ffmpeg->open('video.mpg');
+		// $video
+		//     ->filters()
+		//     ->resize(new FFMpeg\Coordinate\Dimension(320, 240))
+		//     ->synchronize();
+		// $video
+		//     ->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(10))
+		//     ->save('frame.jpg');
+		// $video
+	 //    ->save(new FFMpeg\Format\Video\X264(), 'export-x264.mp4')
+	 //    ->save(new FFMpeg\Format\Video\WMV(), 'export-wmv.wmv')
+	 //    ->save(new FFMpeg\Format\Video\WebM(), 'export-webm.webm');
+
 		$id = Crypt::decrypt($id);  
 		$videos = Video::where('id','=',$id)->get();
 		$fileName = $videos[0]['file_name'];
