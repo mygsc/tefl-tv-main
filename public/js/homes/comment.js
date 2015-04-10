eb63bc25479bb190cf40e32f9c551cff15f7bf68
Rommel Porras <r3mmel023@gmail.com>
2015-04-10 13:28:01 +0800

    Fixed reply image bug

--- public/js/homes/comment.js
+++ public/js/homes/comment.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
         			$("#errorlabel").focus();
         		}
         		if(data['status'] == 'success'){
-        			alert(data['status']);
+        			// alert(data['status']);
         			$('textarea.txtreply').val('');
         			$(this).prepend(data['reply']);
 	        	}
