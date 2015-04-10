531a43443456ba84bcc36740e9f7f6991d57b9a0
Grald <gerald.burasca27@gmail.com>
2015-04-10 09:30:46 +0800

    added error photo

--- app/controllers/VideoController.php
+++ app/controllers/VideoController.php
@@ -34,15 +34,16 @@ class VideoController extends BaseController {
 			$create = Video::create($input);
 			//Find / Updated
 			$latest_id = $create->id;
-			$encrypt_name = $fileName;
+			//$encrypt_name = $fileName;
 			Session::put('fileName', $fileName);
 			$db_filename = Video::find($latest_id);
-			$db_filename->file_name = $encrypt_name;
+			$db_filename->file_name = $fileName;
+			$db_filename->title = 'Untitled';
 			$db_filename->publish = 0;
 
 			if($db_filename->save()){
 				//Start upload
-				$videoFolderPath = $destinationPath. DS. $encrypt_name;
+				$videoFolderPath = $destinationPath. DS. $fileName;
 				if(!file_exists($destinationPath)){
 					mkdir($destinationPath);
 				}
@@ -50,7 +51,7 @@ class VideoController extends BaseController {
 					mkdir($videoFolderPath);
 				}
 				$ext = $file->getClientOriginalExtension();
-				$file->move($videoFolderPath, $encrypt_name.'.'.$ext);  
+				$file->move($videoFolderPath, $fileName.'.'.$ext);  
 				return Response::json([ 'file'=>$fileName]);
 				//return Redirect::route('get.addDescription', $encrypt_name)->with('tokenId', $fileName);
 			}
