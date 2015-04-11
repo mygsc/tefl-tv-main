a84bca74742d83a0389319e6d46efe8b4e1ba641
Grald <gerald.burasca27@gmail.com>
2015-04-11 10:44:09 +0800

    change IP necessary

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '192.168.254.100',
+			'host'      => 'localhost',
 			'database'  => 'tefl-tv',
 			'username'  => 'gil',
 			'password'  => 'gsc2014',
