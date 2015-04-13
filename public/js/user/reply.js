314036669b3292f627b37561bead5c8ecf10775e
Aldrin Gauran <aldrin.gauran@gmail.com>
2015-04-13 14:03:19 +0800

    modified feedbacks2 and reply.js in /channels

--- public/js/user/reply.js
+++ public/js/user/reply.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	$('#userReply').click(function() {
-		$('#replyBox').toggle();
-	});
+$(document).ready(function(){
+    $(".userReply").click(function(){
+      	$(this).find('.replybox').fadeIn();
+    });    
 });
