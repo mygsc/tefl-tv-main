ad69263178e6a60af0746d58dc280b60ea1ed9bf
Grald <gerald.burasca27@gmail.com>
2015-04-15 01:17:00 +0800

    fixed bugs

--- app/views/homes/watch-video.blade.php
+++ app/views/homes/watch-video.blade.php
@@ -196,7 +196,7 @@ window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.
                                     <div class="col-md-11 col-sm-10">
                                         <h2 class="black">
 <<<<<<< HEAD
-                                            <span><a href="http://localhost:8000/channels/{{$owner->channel_name}}">{{ucfirst($owner->channel_name)}}</a> <small>{{count($countSubscribers)}} Subscriber(s)</small>
+                                            <span><a href="//channels/{{$owner->channel_name}}">{{ucfirst($owner->channel_name)}}</a> <small>{{count($countSubscribers)}} Subscriber(s)</small>
                                             @if(Auth::User()->id)
 =======
                                             <span>{{ucfirst($owner->channel_name)}} <small>{{count($countSubscribers)}} Subscriber(s)</small>
