cfccf5c79e7fa8e074b9f6078975cb9054e48f6d
princessadam <princess.adamrebaula@gmail.com>
2015-04-15 13:56:33 +0800

    update index

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,9 +13,9 @@ class HomeController extends BaseController {
 
 	public function getIndex() {
 		$recommendeds = $this->Video->getVideoByCategory('recommended', '6');
-		$populars = $this->Video->getVideoByCategory('popular', '4');
-		$latests = $this->Video->getVideoByCategory('latest', '4');
-		$randoms = $this->Video->getVideoByCategory('random', '4');
+		$populars = $this->Video->getVideoByCategory('popular', '6');
+		$latests = $this->Video->getVideoByCategory('latest', '6');
+		$randoms = $this->Video->getVideoByCategory('random', '6');
 		$categories = $this->Video->getCategory();
 
 		//dd(file_exists('public\videos\4-Cess\Js0zCnwX7XY\Js0zCnwX7XY.jpg'));
--- app/views/homes/index.blade.php
+++ app/views/homes/index.blade.php
@@ -12,18 +12,17 @@
 		
 	@stop
 	@section('css')
-		{{HTML::style('css/vid.player.min.css')}}
+		{{HTML::style('css/vid.player.css')}}
 	@stop
 	@section('some_script')
-	{{HTML::script('js/video-player/media.player.min.js')}}
-	{{HTML::script('js/video-player/fullscreen.min.js')}}
+	{{HTML::script('js/video-player/media.player.js')}}
+	{{HTML::script('js/video-player/fullscreen.js')}}
 @stop
 @section('content')
 <div class="row">
 	<div class="container page2">
 		<BR/>
 		<div class="row same-H">	
-
 			<div class="col-md-6" style="">
 				<div class="">
 					<div class="row  vid-wrapper">
@@ -51,158 +50,146 @@
 	                    <div class="ad2 col-md-12 col-sm-6 col-xs-6">
 	                        <a href="http://www.auathailand.org/"><img src="/img/ads/half-large-rectangle.jpg" class="adDiv"></a>
 	                   </div><!--/.ad2-->
-
-                   </div>
-      
-	
-				<div class="col-md-5 ctgryDiv hidden-sm">
-				<h4>Categories</h4>
-				<span class="">
-					@if(!empty($categories))
-					@foreach($categories as $category)
-						{{$category}}
-					@endforeach
-					@endif
-				</span>
-				</div>
-			</div><!--/.row of col4-->
-		</div>
-
-
-		</div><!--/.row 1st-->
-
-		<br/>
+                    </div>
+					<div class="col-md-5 ctgryDiv hidden-sm">
+						<h4>Categories</h4>
+						<span class="">
+							@if(!empty($categories))
+							@foreach($categories as $category)
+								{{$category}}
+							@endforeach
+							@endif
+						</span>
+					</div>
+				</div><!--/.row of col4-->
+			</div>
+		</div><!--/.same-H-->
 		<!--RECOMMENDED VIDEOS SECTION -->
+		<br/>
 		<div class="row same-H">
 			<div class="categoryHead" style="width:99%!Important">
-
-	            <h3>Recommended Videos</h3>
-	      	</div><!--/.recommended video-->
+				<h3>Recommended Videos</h3>
+			</div><!--/.recommended video-->
 
 			<div class="col-md-12">
 				<div class="row">
-
-				@foreach($recommendeds as $recommended)
-				<a href="{{route('homes.watch-video', array($recommended->file_name))}}">
-		            <div class="col-lg-2 col-md-4 col-sm-6">
-		            	<span class="v-time inline">{{$recommended->total_time}}</span> 	
-		            	<div class="thumbnail-2">
-
-							<img class="hvr-grow-rotate"  src="{{$recommended->thumbnail}}">
-								
-						</div>
-		            	<div class="v-Info">
-		            		<a href="{{route('homes.watch-video', array($recommended->file_name))}}">{{$recommended->title}}</a>
-		            	</div>
-
-		            	<div class="count">
-							by: <a href="{{route('view.users.channel', array($recommended->channel_name))}}">{{$recommended->channel_name}}</a>
-							<br />
-							<i class="fa fa-eye"></i> {{number_format($recommended->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$recommended->likes}} | <i class="fa fa-calendar"></i> {{date('F d, Y',strtotime($recommended->created_at))}}
-						</div>
-		            </div>
-		            </a>
-		        @endforeach
-		        </div>
-	        </div><!--/.col-md-12-->
-		</div><!--/.row for recommended videos-->
-		<br/>
-		<div class="row same-H">
-			<div class="col-md-4">
-				<div class="row">
-					<div class="categoryHead">
-						<h3>Popular</h3>
-					</div>
-
-					@foreach($populars as $popular)
-					<a href="{{route('homes.watch-video', array($popular->file_name))}}">
-					<div class="col-md-6 col-sm-6">
-					<span class="v-time inline">{{$popular->total_time}}</span>
-						<div class="thumbnail-2">
-								<img class="hvr-grow-rotate" src="{{$popular->thumbnail}}">
-						</div>
-						
-						<div class="v-Info">
-							<a href="{{route('homes.watch-video', array($popular->file_name))}}">{{$popular->title}}</a>
-						</div>
-						<div class="count">
-							by: <a href="{{route('view.users.channel', array($popular->channel_name))}}">{{$popular->channel_name}}</a>
-							<br />
-							<i class="fa fa-eye"></i> {{number_format($popular->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$popular->likes}} | <i class="fa fa-calendar"></i> {{date('F d, Y',strtotime($popular->created_at))}}
-						</div>
-					
-					</div>
-					</a>
-					@endforeach
-					<div class="btn-pos">
-						{{ link_to_route('homes.popular', 'see more..', null) }}
-					</div>
-				</div>
-			</div><!--/.col-4 for Popular-->
-			<div class="col-md-4">
-				<div class="row">
-					<div class="categoryHead">
-						<h3>Recent Uploads</h3>
-					</div>
-					@foreach($latests as $latest)
-					<a href="{{route('homes.watch-video', array($latest->file_name))}}">
-						<div class="col-md-6 col-sm-6">
-						<span class="v-time inline">{{$latest->total_time}}</span>
+					@foreach($recommendeds as $recommended)
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<a href="{{route('homes.watch-video', array($recommended->file_name))}}">
+							<span class="v-time inline">{{$recommended->total_time}}</span> 	
 							<div class="thumbnail-2">
-									<img class="hvr-grow-rotate"  src="{{$latest->thumbnail}}">
+								<img class="hvr-grow-rotate"  src="{{$recommended->thumbnail}}">
 							</div>
 							<div class="v-Info">
-								<a href="{{route('homes.watch-video', array($latest->file_name))}}">{{$latest->title}}</a>
+								<a href="{{route('homes.watch-video', array($recommended->file_name))}}">{{$recommended->title}}</a>
 							</div>
-							
-			            	<div class="count">
-								by: <a href="{{route('view.users.channel', array($latest->channel_name))}}">{{$latest->channel_name}}</a>
+							<div class="count">
+								by: <a href="{{route('view.users.channel', array($recommended->channel_name))}}">{{$recommended->channel_name}}</a>
 								<br />
-								<i class="fa fa-eye"></i> {{number_format($latest->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$latest->likes}} | <i class="fa fa-calendar"></i> {{date('F d, Y',strtotime($latest->created_at))}}
+								<i class="fa fa-eye"></i> {{number_format($recommended->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$recommended->likes}} | {{date('F d, Y',strtotime($recommended->created_at))}}
 							</div>
-						</div>
-					</a>
+						</a>
+					</div>
 					@endforeach
-					<div class="btn-pos">
-						{{ link_to_route('homes.latest', 'see more..', null) }}
-					</div>
 				</div>
-			</div><!--/.col-4 for Recent Uploads-->
-
-			<div class="col-md-4">
-				<div class="row">
-					<div class="categoryHead">
-						<h3>Random</h3>
-					</div>
-					@foreach($randoms as $random)
-					<a href="{{route('homes.watch-video', array($popular->file_name))}}">
-					<div class="col-md-6 col-sm-6">
-					<span class="v-time inline">{{$popular->total_time}}</span>
-						<div class="thumbnail-2">
-								<img class="hvr-grow-rotate" src="{{$random->thumbnail}}">
-						</div>
-						<div class="v-Info">
-							<a href="{{route('homes.watch-video', array($random->file_name))}}">{{$random->title}}</a>
-						</div>
-						<div class="count">
-							by: <a href="{{route('view.users.channel', array($random->channel_name))}}">{{$random->channel_name}}</a>
-							<br />
-							<i class="fa fa-eye"></i> {{number_format($random->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$random->likes}} | <i class="fa fa-calendar"></i> {{date('F d, Y',strtotime($random->created_at))}}
-						</div>
-
-					</div>
-					</a>
-					@endforeach
-					<div class="btn-pos">
-						{{ link_to_route('homes.random', 'see more..', null) }}
-					</div>
-				</div>
-			</div><!--/.col-4 for random-->
-
-		</div><!--/.row for threee categories-->
+			</div><!--/.col-md-12-->
 		<br/>
+		</div><!--/.row for recommended videos-->
+		<div class="row same-H">
+			<div class="categoryHead" style="width:99%!Important">
+				<h3>Popular</h3>
+			</div>
+			@foreach($populars as $popular)
+			<div class="col-md-2 col-sm-3">
+				<a href="{{route('homes.watch-video', array($popular->file_name))}}">
+					<span class="v-time inline">{{$popular->total_time}}</span>
+					<div class="thumbnail-2">
+						<img class="hvr-grow-rotate" src="{{$popular->thumbnail}}">
+					</div>
+
+					<div class="v-Info">
+						<a href="{{route('homes.watch-video', array($popular->file_name))}}">{{$popular->title}}</a>
+					</div>
+					<div class="count">
+						by: <a href="{{route('view.users.channel', array($popular->channel_name))}}">{{$popular->channel_name}}</a>
+						<br />
+						<i class="fa fa-eye"></i> {{number_format($popular->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$popular->likes}} | {{date('F d, Y',strtotime($popular->created_at))}}
+					</div>
+				</a>
+			</div>
+			@endforeach
+		</div>
+		<div class="row mg-b-10">
+			<div class="text-center">
+				{{ link_to_route('homes.popular', 'SEE MORE', null, array('class' => 'see-m-v see-m-v-a')) }}
+			</div>
+		</div>
+	
+		<div class="row same-H">
+			<div class="categoryHead" style="width:99%!Important">
+				<h3>Recent Uploads</h3>
+			</div>
+			@foreach($latests as $latest)
+			<div class="col-md-2 col-sm-3">
+				<a href="{{route('homes.watch-video', array($latest->file_name))}}">
+
+					<span class="v-time inline">{{$latest->total_time}}</span>
+					<div class="thumbnail-2">
+						<img class="hvr-grow-rotate"  src="{{$latest->thumbnail}}">
+					</div>
+					<div class="v-Info">
+						<a href="{{route('homes.watch-video', array($latest->file_name))}}">{{$latest->title}}</a>
+					</div>
+
+					<div class="count">
+						by: <a href="{{route('view.users.channel', array($latest->channel_name))}}">{{$latest->channel_name}}</a>
+						<br />
+						<i class="fa fa-eye"></i> {{number_format($latest->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$latest->likes}} | {{date('F d, Y',strtotime($latest->created_at))}}
+					</div>
+				</a>
+			</div>
+			@endforeach
+			
+		</div>
+		<div class="row mg-b-10">
+			<div class="text-center">
+				{{ link_to_route('homes.latest', 'SEE MORE', null, array('class' => 'see-m-v see-m-v-a')) }}
+			</div>
+		</div>
+	
+		<div class="row same-H">
+			<div class="categoryHead" style="width:99%!Important">
+				<h3>Random</h3>
+			</div>
+			@foreach($randoms as $random)
+			<div class="col-md-2 col-sm-3">
+				<a href="{{route('homes.watch-video', array($popular->file_name))}}">
+					<span class="v-time inline">{{$popular->total_time}}</span>
+					<div class="thumbnail-2">
+						<img class="hvr-grow-rotate" src="{{$random->thumbnail}}">
+					</div>
+					<div class="v-Info">
+						<a href="{{route('homes.watch-video', array($random->file_name))}}">{{$random->title}}</a>
+					</div>
+					<div class="count">
+						by: <a href="{{route('view.users.channel', array($random->channel_name))}}">{{$random->channel_name}}</a>
+						<br />
+						<i class="fa fa-eye"></i> {{number_format($random->views,0,null,',')}} | <i class="fa fa-thumbs-up"></i> {{$random->likes}} | {{date('F d, Y',strtotime($random->created_at))}}
+					</div>
+				</a>
+			</div>
+			@endforeach
+			
+		</div>
+		<div class="row mg-b-10">
+			<div class="text-center">
+				{{ link_to_route('homes.random', 'SEE MORE', null, array('class' => 'see-m-v see-m-v-a')) }}
+			</div>
+		</div>
+
+
 	</div><!--/.container page-->
-</div>
+</div><!--first row-->
 @stop
 
 
