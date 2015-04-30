66c9ed5642f66f9a319d9ef3ea417d07dc557de4
geraldburasca.com <gerald.burasca27@gmail.com>
2015-04-30 17:14:19 +0800

    change icon vidoe

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '192.168.1.7',
+			'host'      => 'localhost',
 			'database'  => 'tefl-tv',
-			'username'  => 'gil',
-			'password'  => 'gsc2014',
+			'username'  => 'root',
+			'password'  => 'gerald',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
