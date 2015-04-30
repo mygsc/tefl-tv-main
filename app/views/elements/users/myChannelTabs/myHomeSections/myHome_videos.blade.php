cd3e93b7548273f21cbbe61b31b9775487693369
Aldrin Gauran <aldrin.gauran@gmail.com>
2015-04-30 15:00:22 +0800

    modified UserController, Feedback Model, routes, myhome_videos,channel, feedbacks and reply.js

--- app/views/elements/users/myChannelTabs/myHomeSections/myHome_videos.blade.php
+++ app/views/elements/users/myChannelTabs/myHomeSections/myHome_videos.blade.php
@@ -32,7 +32,7 @@
 								</div>
 							</a>
 							<div class="count">
-								<i class="fa fa-eye"></i> {{$usersVideo->views}} | <i class="fa fa-thumbs-up"></i>  {{$usersVideo->likes}} | {{date('F d, Y',strtotime($$usersVideo->created_at))}}
+								<i class="fa fa-eye"></i> {{$usersVideo->views}} | <i class="fa fa-thumbs-up"></i>  {{$usersVideo->likes}} | {{date('F d, Y',strtotime($usersVideo->created_at))}}
 							</div>
 
 					
