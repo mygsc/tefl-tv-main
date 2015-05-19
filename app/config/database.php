370cd0faa25c4015c5b106814ef6bd5b172bf814
geraldburasca.com <gerald.burasca27@gmail.com>
2015-05-19 16:45:42 +0800

    modified player all ovver again

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 
-			'host'      => '192.168.1.9',
+			'host'      => 'localhost',
 			'database'  => 'tefl-tv',
 			'username'  => 'gil',
 			'password'  => 'gsc2014',
