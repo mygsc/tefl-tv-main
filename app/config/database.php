3a1d20ca8ebde1f0bb14db8131dfe2ff8358d202
princessadam <princess.adamrebaula@gmail.com>
2015-05-19 08:06:36 +0800

    db

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '192.168.254.102',
+			'host'      => '192.168.1.9',
 			'database'  => 'tefl-tv',
 			'username'  => 'gil',
 			'password'  => 'gsc2014',
