f159de8569fdac8d11b59277783e0b93fe1580d3
geraldburasca.com <gerald.burasca27@gmail.com>
2015-05-27 15:34:17 +0800

    modified js

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'tefltv',
+			'database'  => 'tefl-tv',
 			'username'  => 'gil',
 			'password'  => 'gsc2014',
 			'charset'   => 'utf8',
