72f600d3c4e109ab9da53fe74dc6052631b51683
geraldburasca.com <gerald.burasca27@gmail.com>
2015-05-27 13:18:26 +0800

    added css

--- app/views/elements/videoPlayer.blade.php
+++ app/views/elements/videoPlayer.blade.php
@@ -1,35 +1,23 @@
-	<!--advertisement-->
-	<div class="advertisement" id='advertisement'>
-		<div class="span12">
-			<div class="col-md-10 col-md-offset-1">
-				<small class="close">x</small> 
-				<script type="text/javascript">
-
-
-				    google_ad_client = "ca-pub-3138986188138771";
-				    google_ad_slot = "1188440440";
-				    google_ad_width = 728;
-				    google_ad_height = 90;
-				</script>
-				<!-- TEFL TV Normal -->
-				<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js">
-				</script>
-
-			</div>
-
-
-		</div>
-	</div>
-</div>
 <!--advertisement-->
-{{-- <div class="video-ads" id="vid-annotation"></div> --}} 
-{{-- <div id="vid-wrap"> --}}
-<div class="play-icon" id="play-icon">
-	<div class="triangle-icon"></div>
-</div>
-
-
-
+    <div class="advertisement" id='advertisement'>
+       {{--  <div class="span12">
+            <div class="col-md-10 col-md-offset-1"> --}}
+                <div class="close">x</div> 
+                <script type="text/javascript">
+                    google_ad_client = "ca-pub-3138986188138771";
+                    google_ad_slot = "1188440440";
+                    google_ad_width = 728;
+                    google_ad_height = 90;
+                </script>
+                <!-- TEFL TV Normal -->
+                <script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+                </script>
+            {{-- </div>
+        </div>--}}
+    </div>
+	<div class="play-icon" id="play-icon">
+		<div class="triangle-icon"></div>
+	</div>
 	<div class="load-video" id="loader">
 	<div class="spinner2" id='spinner'>
 
@@ -71,7 +59,7 @@
 <div id='ads-hide'>
 	<center><span class='glyphicon glyphicon-chevron-up'></span></center>
 </div>
-{{-- </div> --}}
+
 
 <div class="vd" style="margin-top:-7px;">
 	<div class="wrapper" id="controls">
