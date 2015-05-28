da670709fa5f98f7048a15c2f37aa60e01b0f8bd
Rommel Porras <r3mmel023@gmail.com>
2015-05-28 11:24:18 +0800

    Fixed views bug in watchvideo()

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'tefl-tv',
+			'database'  => 'tefltv_main',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
