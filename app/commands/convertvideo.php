c8e4271bfe8ef169da98db50916a6b7004dfb8a3
Kevin Cabrera <mygraphicstudiocentral@gmail.com>
2015-05-29 20:08:41 -0700

    cool

--- app/commands/convertvideo.php
+++ app/commands/convertvideo.php
@@ -67,12 +67,14 @@ class convertvideo extends Command {
 		if(!empty($videos)){
 			$filename = $videos->file_name;
 			$folderName = $videos->user_id. '-'. $videos->channel_name;
-			$destination = public_path('videos/'. $folderName .DS. $filename);//
+			$destination = public_path('videos/'. $folderName .DS. $filename);
+
 			$source = $destination . DS. 'original' .'.'. $videos->extension;
-			// $this->convertVideoToHigh($source, $destination, $filename);
-			// $this->convertVideoToNormal($source, $destination, $filename);
-			// $this->convertVideoToLow($source, $destination, $filename);
-			$this->convertVideoToDiffFormat($source, $destination, $filename);
+
+			 $this->convertVideoToHigh($source, $destination, $filename);
+			 $this->convertVideoToNormal($source, $destination, $filename);
+			 $this->convertVideoToLow($source, $destination, $filename);
+			//$this->convertVideoToDiffFormat($source, $destination, $filename);
 			$checkFilename = Video::where('file_name',$filename)->first();
 			if($checkFilename->count()){
 				$checkFilename->uploaded = 1;
