1e7c7f3b82e64968ab31b69dfcff2cff2545df90
Kevin Cabrera <cabrerakevinc@yahoo.com>
2015-06-08 14:53:46 +0800

    database

--- app/config/database.php
+++ app/config/database.php
@@ -56,7 +56,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'tefltv_main',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'admin',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
