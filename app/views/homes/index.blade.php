77bed7968e4264aad049ef64b3649f6edd66214d
princessrebaula <princessrebaula@github.com>
2015-06-18 08:11:10 +0800

    update partnerships and home

--- app/views/homes/index.blade.php
+++ app/views/homes/index.blade.php
@@ -33,6 +33,12 @@
 			    			</div>
 						</div><!--/.col-md-6-->
 						<div class="col-md-6	">
+							<div class="center-block text-center same-H" style="background: rgba(255,255,255,0.7);width:100%; padding:5px 5px;margin:0 auto; border:4px solid #f36523;">
+								<h3 class="text-center">Hi User! Do you know that you can earn money by upgrading your channel for FREE?</h3>
+								<a href="{{ route('partnerships.index')}}"><button class="btn btn-primary btn-sm">Learn more</button>
+							</div>
+
+						
 						</div>
 					</div>
 				</div>
