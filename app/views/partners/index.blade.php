e6332bce7c5846f6dd38d9807de217f5ceeb1c53
princessrebaula <princessrebaula@github.com>
2015-06-22 17:18:32 +0800

    updating partner

--- app/views/partners/index.blade.php
+++ app/views/partners/index.blade.php
@@ -1,129 +1,22 @@
-@extends('layouts.partnership')
+@extends('layouts.partner')
 
 @section('title')
 TEFL TV Partner program
 @stop
 
 @section('content')
+<div class="page_wrapper" style="height:100%;">
+	<div class="top-bg"></div>
+</div>
 <div class="container">
-	<div class="row">
-		<div class="col-md-12 White same-H">
-			<br/><br/>
-			<div class="content-padding">
-				<div class="col-md-7 text-justify">
-
-					<h1>What is TEFLTV Partner?</h1>
-					<p class="text-justify">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetur adipisicing elit
-						consequat.
-					</p>
-					<br/>
-					<p class="text-justify">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur.
-					</p>
-				</div>
-				<div class="col-md-5">
-					<br/>
-					<img src="/img/thumbnails/video-big.jpg">
-					<br/><br/>
-				</div>
-				<div class="col-md-12">
-					<h3>Perks of being a TEFLTV Partner</h3>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua, ut labore et dolore magna aliqua.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua ut labore et dolore magna aliqua..
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					</li>
-					<h3>How to become a TEFLTV Partner?</h3>
-					<p class="text-justify">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non
-
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</li>
-					<br/>
-					<li>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua.
-					</li>
-					<br/>
-					<hr/>
-					<h3>Start earning money. <a href="{{route('users.verification')}}">Become a Partner Now!</a></h3>
-					
-					<br/><br/>
-				</div>
-				<div class="col-md-12">
+	<div class="absolute-wrapper">
+		<div style="position:relative!important;width:100%;top:80px;">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="paper same-H" >
 					
 				</div>
 			</div>
-
 		</div>
-		
 	</div>
 </div>
 
