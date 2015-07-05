b9d606f603bafce397b687ccb2081e19cdc50729
princessrebaula <princessrebaula@github.com>
2015-07-06 07:54:20 +0800

    update header

--- app/views/elements/header.blade.php
+++ app/views/elements/header.blade.php
@@ -13,18 +13,21 @@
                 <div class="col-md-6 col-sm-10 text-right col-xs-10">
                     <div class="row">
                         <div class="col-md-10 col-sm-10 col-xs-9">
-                            <div class="mg-t-20">
+                            <div class="mg-t-15">
                             {{Form::open(array('route' => 'homes.searchresult','method' => 'GET', 'style' => ''))}}
                         
                                {{--      {{ Form::select('type',array('Video' => 'Video', 'Playlist' => 'Playlist', 'Channel' => 'Channel'),'Video', array('style' => 'height:20px;', 
                                     'class' => 'cBox'))}} --}}
-                               
-                                {{ Form::text('search', null, array('id' => 'category','required', 'placeholder' => 'Search Video', 'class' => 'form-control col-md-5 input-search')) }}
-                          
-                            {{Form::close()}}
-                            </div>
-                        </div><!--/.col-md-10 search box-->
-
+                                <div class="input-group" style="">
+                                    {{ Form::text('search', null, array('id' => 'category','required', 'placeholder' => 'Search Video', 'class' => 'form-control col-md-5')) }}
+                                    <span class="input-group-btn">
+                                    {{ Form::submit('Search', array('id' => 'button', 'class' => 'btn btn-info ')) }}
+                                    </span>
+                                    {{Form::close()}}
+                                </div>
+                   
+                            </div><!--/.col-md-10 search box-->
+                        </div>      
                         <div class="col-md-2 col-sm-2 col-xs-3">
                             <div class="text-right">
                                 <ul class="ctgryNav pull-right" >
