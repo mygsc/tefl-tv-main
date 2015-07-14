e0be8f0a38ee56710d2947f055a4b59efc9259e0
Rommel Porras <r3mmel023@gmail.com>
2015-07-14 20:51:57 +0800

    Code refactoring in videoComment and Comment()

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -14,9 +14,53 @@ class Comment extends Eloquent {
 
 			$notifications = new Notification();
 			$comments[$key]->time_difference = $notifications->getTimePosted($comment);
+			$comments[$key]->likesCount = DB::table('comments_likesdislikes')->where(array('comment_id' => $comment->id, 'status' => 'liked'))->count();
+			$comments[$key]->dislikeCount = DB::table('comments_likesdislikes')->where(array('comment_id' => $comment->id, 'status' => 'disliked'))->count();
+
+			if(Auth::check()){
+				$comments[$key]->ifAlreadyLiked = DB::table('comments_likesdislikes')->where(array(
+					'comment_id' => $comment->id, 'user_id' => Auth::User()->id,'status' => 'liked'
+				))->first();
+				$comments[$key]->ifAlreadyDisliked = DB::table('comments_likesdislikes')->where(array(
+					'comment_id' => $comment->id, 'user_id' => Auth::User()->id,'status' => 'disliked'
+				))->first();
+			}
+
+			$comments[$key]->getCommentRepliesCount = DB::table('comments_reply')
+			->join('users', 'users.id', '=', 'comments_reply.user_id')
+			->where('comment_id', $comment->id)->count();
+
+			$comments[$key]->getCommentReplies = CommentReply::select('comments_reply.id', 'user_id', 'reply', 'comment_id',
+				'comments_reply.created_at', 'comments_reply.updated_at', 'channel_name')
+			->join('users', 'users.id', '=', 'comments_reply.user_id')
+			->get();	
+
+			/***Comment Replies Section***/
+			foreach($comments[$key]->getCommentReplies as $key1 => $getCommentReplies1){
+				$profile_picture =	User::getUsersImages($getCommentReplies1->user_id)['profile_picture'];
+				$comments[$key]->getCommentReplies[$key1]->profile_picture = $profile_picture;
+
+				$notifications = new Notification();
+				$comments[$key]->getCommentReplies[$key1]->time_difference = $notifications->getTimePosted($comment);
+
+				$comments[$key]->getCommentReplies[$key1]->likesCountReply = DB::table('comments_reply_likesdislikes')->where(array('comments_reply_id' => $comment->id, 'status' => 'liked'))->count();
+				$comments[$key]->getCommentReplies[$key1]->dislikeCountReply = DB::table('comments_reply_likesdislikes')->where(array('comments_reply_id' => $comment->id, 'status' => 'disliked'))->count();
+
+				if(Auth::check()){
+					$comments[$key]->getCommentReplies[$key1]->ifAlreadyLiked = DB::table('comments_reply_likesdislikes')->where(array(
+						'comments_reply_id' => $comment->id, 'user_id' => Auth::User()->id,'status' => 'liked')
+					)->first();
+					$comments[$key]->getCommentReplies[$key1]->ifAlreadyDisliked = DB::table('comments_reply_likesdislikes')->where(array(
+						'comments_reply_id' => $comment->id, 'user_id' => Auth::User()->id,'status' => 'disliked')
+					->first();
+				}
+			}
+			/***Comment Replies Section***/
 		}
+
 		return $comments;
 	}
+
 	public function countLikesAndComments($id,$totalLikes=0,$totalDislikes=0){
 		$result = Comment::where('video_id',$id)->get();
 		if(!$result->isEmpty()){
