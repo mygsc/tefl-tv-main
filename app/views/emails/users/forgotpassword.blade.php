550aa381f3eabe1890264e623716994a30b42794
Kevin Cabrera <mygraphicstudiocentral@gmail.com>
2015-07-15 02:10:22 -0700

    emails

--- app/views/emails/users/forgotpassword.blade.php
+++ app/views/emails/users/forgotpassword.blade.php
@@ -1,57 +1,51 @@
-
-<!--<div style="background:#f1f1f1;padding:20px 20px;">
-	<table>
-		<tr>
-			<td width="30%" style="background:#FFF;padding:10px 20px;border-radius:10px 0 0 10px;">
-				<div style="width:100%; height:150px;">
-					<img src="http://www.test.tefltv.com/img/logos/teflTv.png" style="width:150px;height:auto;margin-left:20px;">
-				</div>
-			</td>
-			<td style="width:70%; background:#D1DFF9;border-left:20px solid #1F3359;border-radius:0 10px 10px 0;">
-				<div style="width:720px; height:150px; padding:20px 20px;">
-					Hi {{$first_name}}!
-					<br><br>
-					Please click the link below to reset your password
-					<br>
-					<a href="{{$url}}">{{$url}}</a>
-				</div>
-			</td>
-		</tr>
-	</table>
-</div>-->
 <div style="background:#f1f1f1;padding:20px 20px;">
 <table width="80%" cellspacing="0" cellpadding="0" border="0" align="center">
 	<tbody>
 		<tr>
-			<td style="background:#1f3359;height:80px">
-				<div style="padding:7px 10px">
-					<img src="http://www.test.tefltv.com/img/email/logo-e.png"  />
+			<td style="background:#1f3359;height:70px">
+				<div style="padding:0 10px">
+					<img src="https://ci4.googleusercontent.com/proxy/l4h8nMCV5k7yPpo-c-wK67PJwK-1E9948TmPCrMfQa5l_Sa2xGuDIB-2OKkxLb1bjoT8ltmBCgMs6ht-dvE0wbRLDfx9=s0-d-e1-ft#http://www.test.tefltv.com/img/email/logo-e.png" style="width: 250px;min-height:auto" class="CToWUd">
 				</div>
 			</td>
 		</tr>
 		<tr>
 			<td style="background:#d1dff9;height:30px">
-				<div style="padding:10px 10px">
-					<a href="" style="color:#3b5a9b; text-decoration: none;">Popular</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="" style="color:#3b5a9b; text-decoration: none;">Latest</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="" style="color:#3b5a9b; text-decoration: none;">Playlist</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<a href="" style="color:#3b5a9b; text-decoration: none;">Channels</a>
+				<div style="padding:5px 10px">
+					<a href="https://www.tefltv.com/popular" style="color:#3b5a9b; text-decoration: none;">Popular</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.tefltv.com/latest" style="color:#3b5a9b; text-decoration: none;">Latest</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.tefltv.com/playlist" style="color:#3b5a9b; text-decoration: none;">Playlist</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.tefltv.com/top-channels" style="color:#3b5a9b; text-decoration: none;">Channels</a>
 				</div>
 			</td>
 		</tr>
 		<tr>
 			<td style="background:#fff;height:400px;">
-				<div style="padding:20px 40px;">
-					<p style="font-size:14px;font-weight: bold;">Hi {{$first_name}}!</p>
-					<br><br>
-					Please click the link below to reset your password
+				<div style="padding:40px 40px;">
+					<h2 style="text-align:center;color:#f18200;font-weight:normal;">TEFLtv Password Reset</h2>
+					<hr/>
+					<br/>
+					<p style="font-size:18px;color:#4B5B7C;">Dear {{$first_name}},</p>
 					<br>
-					<a href="{{$url}}">{{$url}}</a>
-				</div>
+					<p style="font-size:16px;color:#3C3C3C!important;text-align:justify;">
+						We recently received a request to reset your password on TEFLtv.
+					</p>
+					<p style="font-size:16px;color:#3C3C3C!important;text-align:justify;">
+					To reset your password, please click the link below:
+					</p>
+					<p>
+						<a href="{{$url}}">{{$url}}</a>
+					</p>
+					<p style="font-size:16px;color:#3C3C3C!important;text-align:justify;">If clicking the link doesn’t work, you can copy and paste the link into your browser’s address window or retype it there. Please note that the link above is only valid for 24 hours.</p>
+					<p style="font-size:16px;color:#3C3C3C!important;text-align:justify;">If you didn't perform this request, please ignore this email. Rest assured your account is safe with us.</p>
+					<br/>
+					<p style="font-size:18px;color:#4B5B7C;">Regards,<br/>
+						<b>TEFLtv Customer Support Team</b>
+					</p>
+			 	</div>
 			</td>
 		</tr>
 		<tr>
-			<td style="background:#636ea6;height:15px;">
+			<td style="background:#252c44;height:15px;">
 				<div style="padding:0 10px">
 					<p style="text-align:center;color:#ffffff;font-size:10px;">ALL RIGHTS RESERVED 2015</p>
 				</div>
@@ -60,3 +54,5 @@
 	</tbody>
 </table>
 </div>
+
+
