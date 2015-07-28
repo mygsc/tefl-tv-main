c39695662da792d93e7a2f716dcd89c47c7f9241
Grald <gerald.burasca27@gmail.com>
2015-07-28 20:39:42 +0800

    added meta

--- app/views/homes/watch-video.blade.php
+++ app/views/homes/watch-video.blade.php
@@ -9,6 +9,7 @@
         <meta property="og:site_name" content="TEFL-TV">
         <meta property="og:url" content="{{asset('/')}}watch?v={{$videos->file_name}}">
         <meta property="og:title" content="{{$videos->title}}">
+        <meta property="og:image" content="{{asset('/')}}videos/{{$videos->user_id}}-{{$owner->channel_name}}/{{$videos->file_name}}/{{$videos->file_name}}.jpg">
         <meta property="og:image" content="{{asset('/')}}videos/{{$videos->user_id}}-{{$owner->channel_name}}/{{$videos->file_name}}/{{$videos->file_name}}_600x338.jpg">
         <meta property="og:description" content="{{$videos->description}}">
         <meta property="og:type" content="video"> 
@@ -19,9 +20,9 @@
         <meta property="og:video:height" content="360"> 
         <meta property="og:video:tag" content="{{$videos->tags}}"> 
         <meta name="description" content="{{$videos->description}} watch our tefl videos for the best esl community">
-        <meta name="keywords" content="{{$videos->tags}}">
+        <!-- <meta name="keywords" content="{{$videos->tags}}">
         <meta property="article:author" content="{{$owner->channel_name}}">
-        <meta property="article:publisher" content="{{$owner->channel_name}}"/>
+        <meta property="article:publisher" content="{{$owner->channel_name}}"/> -->
     
 @stop
 @section('css')
