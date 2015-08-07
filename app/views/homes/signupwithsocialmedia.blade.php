28bb1749d086ef0c25a281342ce99c18f3b32026
Kevin Cabrera <mygraphicstudiocentral@gmail.com>
2015-08-06 23:58:16 -0700

    update

--- app/views/homes/signupwithsocialmedia.blade.php
+++ app/views/homes/signupwithsocialmedia.blade.php
@@ -1,60 +1,66 @@
-@extends('layouts.default')
+@extends('layouts.signin')
 @section('title')
 	Sign up with {{strtoupper(Session::get('social_media'))}}
 @stop
 @section('content')
-<div class="row White">
-	<div class='container'>
-		<br/><br/>
-		<div class="row">
-			<div class="col-md-8 col-sm-7">
+<style>
+	b{
+		font-size:12px;
+	}
+</style>
+<div class="row" >
+    <div id="cmn-video-demo3__container" style="z-index:-1;">
+        <video id="cmn-video-demo3__video" autoplay muted="muted" loop="true">
+            <source src="/videos/tefltv.mp4" type="video/mp4">
+            <source src="/videos/tefltv.webm" type="video/webm">
+            <source src="/videos/tefltv.ogg" type="video/ogg">
+            Your browser doesn't support HTML5 video. Here's a <a href="#">link</a> to download the video.
+        </video>
+        <div id="cmn-video-demo3__content" >
+        </div>
+    </div>
+    <div class="absolute-wrap">
+	    <div class="container" >
+			<div class="row" style="">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<div class="sign-log-wrapp2 box-shadow">
+						@if(Session::get('social_media') == 'facebook')
+						<img src="/img/icons/fb.png" class="f-icon center-block" style="width:40px;height:40px;">
+						<div id="status" class="text-center connectTo c-fb">
+							<h3 href="social/facebook" class="whiteC">Signup with Facebook</h3>
+						</div>
+						<div class="well2 text-center">
+							<p class="">This are the information that we gathered from your
+								<a href="http://facebook.com/{{Session::get('social_media_id')}}">
+									{{strtoupper(Session::get('social_media'))}} account
+								</a>
+							</p>
+							@else
+							<img src="/img/icons/google.png" class="g-icon center-block" style="width:40px;height:40px;">
+							<div id="status" class="text-center connectTo c-gp">
+								<h3 href="social/google" class="whiteC">Signup with Google</h3>
+							</div>
 
-				<div class="row sign-upLinks text-center ">
-					<h1 class="text-center">
-						Create an account for FREE!
-						<br/>
-						<small class="blackC">Choose how to sign up</small>
-					</h1>
-					<a href = "{{route('homes.googleconnect',array('action' => 'signup'))}}"><img src="/img/icons/google.png" class="social-roll google"></a>
-					<a href="{{route('homes.signin', array('signup' => 'signup'))}}"><img src="/img/icons/tefltv.png" class="social-roll tefltv signBtn"></a>
-					<a href="{{route('homes.facebookconnect',array('action' => 'signup'))}}"><img src="/img/icons/fb.png" class="social-roll fb "></a>
-				</div>
-
-			</div>
-			<div class="col-md-4 col-sm-5">
-				<div class="signDivH textbox-layout2 same-H animated zoomIn">
-							@if(Session::get('social_media') == 'facebook')
-								<img src="/img/icons/fb.png" class="f-icon center-block">
-								<div id="status" class="text-center connectTo c-fb">
-                                    <h2 href="social/facebook" class="whiteC">Signup with Facebook</h2>
-                                </div>
-								<div class="well2 text-center">
+							<div class="well2 text-center">
 								<p class="">This are the information that we gathered from your
-									<a href="http://facebook.com/{{Session::get('social_media_id')}}">
+									<a href="http://plus.google.com/{{Session::get('social_media_id')}}">
 										{{strtoupper(Session::get('social_media'))}} account
 									</a>
 								</p>
-							@else
-								<img src="/img/icons/google.png" class="g-icon center-block">
-								<div id="status" class="text-center connectTo c-gp">
-                                    <h2 href="social/google" class="whiteC">Signup with Google</h2>
-                                </div>
-							
-								<div class="well2 text-center">
-									<p class="">This are the information that we gathered from your
-										<a href="http://plus.google.com/{{Session::get('social_media_id')}}">
-											{{strtoupper(Session::get('social_media'))}} account
-										</a>
-									</p>
-							
-							@endif
-						<span>First Name: {{Session::get('first_name')}}</span><br />
-						<span>Last Name: {{Session::get('last_name')}}</span><br />
-						<span>Email: {{Session::get('email')}}</span><br />
+
+								@endif
+								<span><b>First Name: </b>{{Session::get('first_name')}}</span><br />
+								<span><b>Last Name: </b>{{Session::get('last_name')}}</span><br />
+								<span><b>Email: </b>{{Session::get('email')}}</span><br />
+
+							</div>
+
 						</div>
-						<hr/>
+					</div>
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<div class="signDivH textbox-layout2 same-H animated zoomIn sign-log-wrapp">
 						<h3 class="text-center orangeC">-To complete signup, please fill out the fields.-</h3>
-						<hr/>
+	
 						{{Form::open(array('route' => 'post.signupwithsocialmedia'))}}
 					
 						{{Form::hidden('first_name', Session::get('first_name'))}}
@@ -62,20 +68,18 @@
 						{{Form::hidden('email', Session::get('email'))}}
 
 						<div class="textbox-layouts">
-							{{Form::label('channel_name', 'Username')}}
-							{{Form::text('channel_name','', array('class' => 'form-control'))}}
+							{{Form::text('channel_name','', array('class' => 'form-control', 'placeholder' => 'Channel Name'))}}
 							<span class="inputError">
 								{{$errors->first('channel_name')}}
 							</span>
 
-							{{Form::label('password', 'Password')}}
-							{{Form::password('password','', array('class' => 'form-control'))}}
+							{{Form::password('password',array('class' => 'form-control txt_password', 'placeholder' => 'Password'))}}
+							
 							<span class="inputError">
 								{{$errors->first('password')}}
 							</span>
 							<br/>
-							{{Form::label('confirm_password', 'Confirm Password')}}
-							{{Form::password('confirm_password','', array('class' => 'form-control'))}}
+							{{Form::password('confirm_password', array('class' => 'form-control txt_password', 'placeholder' => 'Confirm Password'))}}
 							
 							<span class="inputError">
 								{{$errors->first('confirm_password')}}
@@ -88,11 +92,14 @@
 						</div>
 
 						{{Form::close()}}
-	
+						</div>
+
+
 				</div>
-			</div>
-		</div>
-	</div>
+	</div><!--/.container page-->
+
 </div>
+</div>
+
 
 @stop
