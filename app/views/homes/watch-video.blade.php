fe3fc6be2a5debf1a9081efd0f786421d03055bb
Grald <gerald.burasca27@gmail.com>
2015-08-11 13:57:51 +0800

    added video

--- app/views/homes/watch-video.blade.php
+++ app/views/homes/watch-video.blade.php
@@ -14,18 +14,6 @@
     ?>
     <meta property="og:image" content="{{asset('/')}}videos/{{$videos->user_id}}-{{$owner->channel_name}}/{{$videos->file_name}}/{{$videos->file_name}}_600x338.jpg"/>
     <meta property="og:description" content="{{htmlentities($videos->description)}}"/>
-    
-    
-   {{-- 
-    <meta property='og:video' content='{{asset('/')}}tefltv_fl_video_player/tefltv_video_player.swf'/>
-    <!-- <meta property='og:video:url' content='{{asset('/')}}sharing/{{$videos->file_name}}'/> -->
-    <meta property='og:video:secure_url' content='https://www.tefltv.com/tefltv_fl_video_player/tefltv_video_player.swf?'/>
-    <meta property="og:video:type" content="application/x-shockwave-flash">
-    <meta property="og:video:width" content="600"/> 
-    <meta property="og:video:height" content="360"/> 
-    <meta name="description" content="{{htmlentities($videos->description)}} watch our tefl videos for the best esl community"/>
-    <meta name="keywords" content="{{$videos->tags}}"/>  --}} 
-
     <meta property="og:type" content="video"/> 
     <meta property="og:video:url" content="https://www.tefltv.com/embed/{{$videos->file_name}}">
     <meta property="og:video:secure_url" content="https://www.tefltv.com/embed/{{$videos->file_name}}">
