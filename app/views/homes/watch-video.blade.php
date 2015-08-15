21585ac02c9ab95af3f73b29c12e82a305be3b2c
Grald <gerald.burasca27@gmail.com>
2015-08-15 16:31:55 +0800

    added video source

--- app/views/homes/watch-video.blade.php
+++ app/views/homes/watch-video.blade.php
@@ -23,10 +23,9 @@
     <meta property="og:video:type" content="video/mp4"/>
     <meta property="og:video:width" content="640"/>
     <meta property="og:video:height" content="360"/>
-    <meta property="og:video:url" content="https://www.tefltv.com/sharing/{{$videos->file_name}}"/> 
-     <meta property="og:video:secure_url" content="https://www.tefltv.com/sharing/{{$videos->file_name}}"/> 
-    <!-- <meta property="og:video" content="https://www.tefltv.com/tefltv_fl_video_player/tefltv_video_player.swf?file=https://www.tefltv.com/videos/tefltv.flv"/>
-    <meta property="og:video:secure_url" content="https://www.tefltv.com/tefltv_fl_video_player/tefltv_video_player.swf?file=https://www.tefltv.com/videos/tefltv.flv"/> -->
+    <meta property="og:video" content="https://www.tefltv.com/sharing/{{$videos->file_name}}"/> 
+    <meta property="og:video" content="https://www.tefltv.com/tefltv_fl_video_player/tefltv_video_player.swf?file=https://www.tefltv.com/videos/tefltv.flv"/>
+    <meta property="og:video:secure_url" content="https://www.tefltv.com/tefltv_fl_video_player/tefltv_video_player.swf?file=https://www.tefltv.com/videos/tefltv.flv"/>
     <meta property="og:video:type" content="application/x-shockwave-flash"/>
     <meta property="og:video:width" content="640"/>
     <meta property="og:video:height" content="360"/> 
