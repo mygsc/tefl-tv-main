dc7f467aa820770058863dffc572a70e2d1db481
Kevin Cabrera <mygraphicstudiocentral@gmail.com>
2015-08-17 18:31:13 -0700

    update

--- app/views/homes/video_reported.blade.php
+++ app/views/homes/video_reported.blade.php
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 4a75f3824dee4638b7f7674d850e31617859ed5a
 @extends('layouts.default')
 
 @section('title')
@@ -31,20 +28,7 @@
     <div class="">
         <div class="row mg-t-10">
             <div id="featured" > 
-<<<<<<< HEAD
-                <div class="col-md-8">
-                    <div class="">
-                        <div id="" class="ui-tabs-panel White pad-s-20 same-H" style="">
-                            <!--video paler-->
-                            <br/>
-                            <div id='ablockVideoPlayer'>
-                                <h2><b>This video has been removed because its content violated TEFL TV's Terms of Service.</b></h2>
-                                <hr/>
-                                Sorry about that.
-                                <br/>
-                                <center>
-                                    <img id="ablockplayer_img" src="/img/nav-effect-b.png" />
-=======
+
                 <br/><br/><br/><br/><br/><br/>
                 <div class="col-md-10 col-md-offset-1">
                     <div class="">
@@ -57,7 +41,7 @@
                                 <hr/>
                                 <center>
                                    
->>>>>>> 4a75f3824dee4638b7f7674d850e31617859ed5a
+
                                 </center>
                             </div>
                             <br/>
