222d0d7ade4925634be3306d6f2a10c4cc0e043a
Grald <gerald.burasca27@gmail.com>
2015-08-18 09:43:00 +0800

    update path

--- app/views/homes/watch-video.blade.php
+++ app/views/homes/watch-video.blade.php
@@ -356,7 +356,7 @@ window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.
                                     <div class="show_wrapp">
                                         <div class=" col-middle">
                                             @if(file_exists(public_path("/videos/".$relation['uid']."/".$relation['file_name']."/".$relation['file_name'].".jpg")))
-                                                <div class="showme" style="background:url(/videos/{{$relation['uid']}}-{{$relation['channel_name']}}/{{$relation['file_name']}}/{{$relation['file_name']}}.jpg);background-size:100% auto;height:100%!important;" >     
+                                                <div class="showme" style="background:url(/videos/{{$relation['uid']}}/{{$relation['file_name']}}/{{$relation['file_name']}}.jpg);background-size:100% auto;height:100%!important;" >     
                                             @else
                                                 <div class="showme" style="background:url(/img/thumbnails/video.png);background-size:100% auto;">
                                             @endif
@@ -375,7 +375,7 @@ window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.
                                     <div class="row-same-height" title="">
                                         <div class="col-md-5 col-xs-4 col-md-height col-middle">
                                             @if(file_exists(public_path("/videos/".$relation['uid'].$relation['file_name']."/".$relation['file_name'].".jpg")))
-                                                <img src="/videos/{{$relation['uid']}}-{{$relation['channel_name']}}/{{$relation['file_name']}}/{{$relation['file_name']}}.jpg" alt="" width="100%" />
+                                                <img src="/videos/{{$relation['uid']}}/{{$relation['file_name']}}/{{$relation['file_name']}}.jpg" alt="" width="100%" />
                                             @else
                                                 <img src="/img/thumbnails/video.png" alt="" width="100%" />
                                             @endif
