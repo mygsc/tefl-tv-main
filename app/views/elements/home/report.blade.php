4fa9de8814053ea2d51bfc8127c16349ded2690c
Kevin Cabrera <mygraphicstudiocentral@gmail.com>
2015-08-19 02:52:04 -0700

    Modify app - LIVE

--- app/views/elements/home/report.blade.php
+++ app/views/elements/home/report.blade.php
@@ -1,17 +1,18 @@
-@if(Auth::check())
-	<div class="panel panel-info same-H ">
-		<div class="panel-heading" role="tab" id="headingTwo">
-			<p class="panel-title whiteC">
-				<a href="">
-					<i class="fa fa-user"></i> Reports
-				</a>
-			</p>
-		</div>
-		<div id="myChannel" >
-			<div class="panel-body">
+
+<div class="panel panel-info same-H ">
+	<div class="panel-heading" role="tab" id="headingTwo">
+		<p class="panel-title whiteC">
+			<a href="">
+				<i class="fa fa-user"></i> Reports
+			</a>
+		</p>
+	</div>
+	<div id="myChannel" >
+		<div class="panel-body">
+			@if(Auth::check())
 				<li role="presentation">{{link_to_route('get.myreports', 'My Reports')}}</li>
-				<li role="presentation">{{link_to_route('get.complaint_form', 'Complaint Form')}}</li>
-			</div>
+			@endif
+			<li role="presentation">{{link_to_route('get.complaint_form', 'Complaint Form')}}</li>
 		</div>
 	</div>
-@endif
+</div>
