181f0318f376b072a097f10a5ead018d2becfe73
Kevin Pogi <cabrerakevinc@yahoo.com>
2015-08-26 17:03:06 +0800

    update

--- app/views/elements/home/uploaderLatestVideo.blade.php
+++ app/views/elements/home/uploaderLatestVideo.blade.php
@@ -18,7 +18,7 @@
                 <a href="/watch?v={{$ownerVideo->file_name}}" class="">
                     <div class="thumbnail-2">
                         @if(file_exists(public_path("/videos/".$ownerVideo->user_id."/".$ownerVideo->file_name."/".$ownerVideo->file_name.".jpg")))
-                            <img class="hvr-grow-rotate" src="/videos/{{$ownerVideo->user_id}}-{{$owner->channel_name}}/{{$ownerVideo->file_name}}/{{$ownerVideo->file_name}}.jpg" alt="" width="100%" />
+                            <img class="hvr-grow-rotate" src="/videos/{{$ownerVideo->user_id}}/{{$ownerVideo->file_name}}/{{$ownerVideo->file_name}}.jpg" alt="" width="100%" />
                             @else
                             <img class="hvr-grow-rotate" src="/img/thumbnails/video.png" alt=""  width="100%"/>
                         @endif
