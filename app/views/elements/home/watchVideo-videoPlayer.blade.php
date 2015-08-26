843e626fb005e3e23372bd67f64d0ef1ebeb1fd6
Kevin Pogi <cabrerakevinc@yahoo.com>
2015-08-26 16:08:25 +0800

    update

--- app/views/elements/home/watchVideo-videoPlayer.blade.php
+++ app/views/elements/home/watchVideo-videoPlayer.blade.php
@@ -3,8 +3,8 @@
 		<div class="row" id="vid-wrapper">
 			<div id="vid-controls">
 				<div class="embed-responsive embed-responsive-16by9">
-					@if(file_exists(public_path('/videos/'.$videos->user_id.$videos->file_name.'/'.$videos->file_name.'.jpg')))
-					<video preload="auto" id="media-video" width="100%" poster="/videos/{{$videos->user_id}}-{{$owner->channel_name}}/{{$videos->file_name}}/{{$videos->file_name}}_600x338.jpg" class="embed-responsive-item">
+					@if(file_exists(public_path('/videos/'.$videos->user_id.'/'.$videos->file_name.'/'.$videos->file_name.'.jpg')))
+					<video preload="auto" id="media-video" width="100%" poster="/videos/{{$videos->user_id}}/{{$videos->file_name}}/{{$videos->file_name}}_600x338.jpg" class="embed-responsive-item">
 						<source id='mp4' src='/videos/{{$videos->user_id}}/{{$videos->file_name}}/{{$videos->file_name}}.mp4' type='video/mp4'>
 						<source id='webm' src='/videos/{{$videos->user_id}}/{{$videos->file_name}}/{{$videos->file_name}}.webm' type='video/webm'>
 					</video>
