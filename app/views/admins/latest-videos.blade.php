ef0ff2d17b0758135cf102fd36b46419bd2f64bf
Kevin Pogi <cabrerakevinc@yahoo.com>
2015-10-20 14:53:06 +0800

    update added likes count on description of the video

--- app/views/admins/latest-videos.blade.php
+++ app/views/admins/latest-videos.blade.php
@@ -11,6 +11,7 @@ Latest Videos
 <img src="{{$video->thumbnail}}">
 title: <a href="{{route('homes.watch-video', 'v='.$video->file_name)}}">{{$video->title}}</a> <br/>
 description: ..... <br/>
+likes: {{$video->like}}
 views: {{$video->views	}} <br/>
 date: {{$video->created_at}}
 </div>
